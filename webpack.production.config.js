3c06a08cad27c0c8b5e4491d7d31569ef3fc5f3e
mikethehud <mail@mikehudson.de>
2018-01-06 02:24:32 +0100

    Webpack config for umd bundling

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,12 +7,27 @@ var webpack = require('webpack');
 module.exports = {
   entry: "./src/react-selekt.js",
   output: {
+    library: 'ReactSelekt',
+    libraryTarget: 'umd',
     path: __dirname + "/dist/",
     filename: "react-selekt.js"
   },
+  externals: [
+    {
+      "react": {
+        root: "React",
+        commonjs2: "react",
+        commonjs: "react",
+        amd: "react"
+      }
+    }
+  ],
   module: {
     loaders: [
       { test: /\.js?$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
   }
 };
